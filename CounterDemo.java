28391995533c186dbe794b50515ffa3b1951de9f
Vaishnavi Patil <93943926+vpvaish@users.noreply.github.com>
2024-02-27 19:53:52 +0530

    Add files via upload

---
+++ CounterDemo.java
@@ -0,0 +1,23 @@
+package Static_Keyword;
+
+public class CounterDemo {
+	
+	static int count=0;
+	
+	CounterDemo(){
+		count++;
+		System.out.println(count);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		CounterDemo C1 = new CounterDemo();
+//		CounterDemo C2 = new CounterDemo();
+//		CounterDemo C3 = new CounterDemo();
+//		CounterDemo C4 = new CounterDemo();
+//		
+		
+	}
+
+}
